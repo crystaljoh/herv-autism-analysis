e2a05b35d6ec3d4bc40563e959bae514300a8412
Duncan MacLean <dmaclean@acm.org>
2026-06-09 19:59:50 +0000

    add version to some accession numbers to get the EDirect retrieval from genbank to work

--- allofus/blat/all_hervs/fetch_herv.sh
+++ allofus/blat/all_hervs/fetch_herv.sh
@@ -1,1 +1,7 @@
-esearch -db nucleotide -query $1 | efetch -format fasta > $1.fa
+# apply this script to the list of accession numbers in hervs_to_fetch_from_genbank e.g.
+# cat hervs_to_fetch_from_genbank | xargs -n 1 ./fetch_herv.sh
+# This typically procduces a fair bit of error output.
+# It also sometimes fails to fetch the fasta, resulting in a 0 length output file.
+# If this happens, checking the revision number at genbank and including that
+# may fix the problem e.g. AF164611.1
+esearch -db nuccore -query $1 | efetch -format fasta > $1.fa
