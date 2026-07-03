8c6b1e8b2a59a81cbaf718f2c5970cd93f1dde2d
Duncan MacLean <dmaclean@acm.org>
2026-07-03 00:20:53 +0000

    create complete intermediate gene file for troubleshooting

--- allofus/retrotector/processretro.sh
+++ allofus/retrotector/processretro.sh
@@ -5,8 +5,10 @@ if [ $participant_id = "ref" ]
 then
     haplotype=0
 fi
-find Workplace_${participant_id}_${chromosome}_${haplotype} -name 'Putein*' -print -exec egrep 'Starts at position|For fit to alignment|{ Gene:|MostUsedRow' {} \; | awk -v chromosome=${chromosome} -f ../../merge_lines.awk  | sort -n -k 1,1 | awk -f ../../filter_env.awk > Workplace_${participant_id}_${chromosome}_${haplotype}/genes.txt
+dir=Workplace_${participant_id}_${chromosome}_${haplotype}
+find ${dir} -name 'Putein*' -print -exec egrep 'Starts at position|For fit to alignment|{ Gene:|MostUsedRow' {} \; | awk -v chromosome=${chromosome} -f ../../merge_lines.awk  | sort -n -k 1,1 > ${dir}/genes.txt
+cat ${dir}/genes.txt | awk -f ../../filter_env.awk > ${dir}/filtered_genes.txt
 if [ $participant_id != "ref" ]
 then
-    awk  -v slack=${slack:=60000} -f ../../find_matches.awk Workplace_ref_${chromosome}_0/genes.txt Workplace_${participant_id}_${chromosome}_${haplotype}/genes.txt | sort -n -k 2,2 > Workplace_${participant_id}_${chromosome}_${haplotype}/mismatches.txt 
+    awk  -v slack=${slack:=60000} -f ../../find_matches.awk Workplace_ref_${chromosome}_0/filtered_genes.txt ${dir}/filtered_genes.txt | sort -n -k 2,2 > ${dir}/mismatches.txt 
 fi
